f16010edac95366d004512e83108f3697e1f01b9
morrisco <morriscotech@gmail.com>
2025-05-28 15:07:33 +0100

    cors enabled

--- server.js
+++ server.js
@@ -22,7 +22,10 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://adeleempowermentfoundation.org',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
